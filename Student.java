7f811dcf3b6031add3618c2fa5c40f0f671a3a35
sirichowdary123 <sirichowdary8742gmail.com>
2021-11-23 12:24:02 +0530

    commited the 2nd file

---
+++ Student.java
@@ -0,0 +1,13 @@
+ class Student{
+    public static void main(String[] args){
+        
+        int x=20;
+        int y=30;
+        if(x+y>50){
+            System.out.println("x+y is grater than 50");
+        }
+         else{
+             System.out.println("x+y is less than 50");
+         }  
+        }
+    }
