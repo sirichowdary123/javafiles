37fa40bd62cbd6dc590cc66ea4cb72b1bbc30f47
sirichowdary123 <sirichowdary8742gmail.com>
2021-11-25 08:50:59 +0530

    commited a new file named Stdin

---
+++ Stdin.java
@@ -0,0 +1,17 @@
+ import java.util.Scanner;
+ public class Stdin {
+     public static void main(String[] args){
+         Scanner in = new Scanner(System.in);
+         int N = in.nextInt();
+
+          Double D = in.nextDouble();
+          
+          String data = in.next();
+          System.out.println("String:"+data);
+          System.out.println("Double:"+D);
+          System.out.println("int:"+N);
+
+
+     }
+    
+}
