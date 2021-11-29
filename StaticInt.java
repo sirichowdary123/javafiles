34986c343dc6b687c879241ac863868b6953073e
sirichowdary123 <sirichowdary8742gmail.com>
2021-11-29 22:18:34 +0530

    added the day 3 file named StaticInt

---
+++ StaticInt.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import java.math.*;
+public class StaticInt {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int B = sc.nextInt();
+        int H = sc.nextInt();
+        
+         if(B<=0)
+          
+            System.out.println("java.lang.Exception:Breadth and height must be positive");
+          
+           if(H<=0)
+           
+            System.out.println("java.lang.Exception:Breadth and height must be positive");
+        
+
+        {
+            System.out.println(B*H);
+        
+        }
+           
+         
+    }    
+    
+}
