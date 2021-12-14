c45ca921651622fb72b12571ce4a503315958bb0
sirichowdary123 <sirichowdary8742gmail.com>
2021-12-14 22:24:16 +0530

    added pattern4 file

---
+++ Pattern4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+public class Pattern4 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+       int N= sc.nextInt();
+        // C = sc.nextInt();
+        //int D = sc.nextInt();
+        
+        for(int i=1;i<=N;i++){
+           //int ch =65;
+        for(int j=1;j<=i;j++){
+            System.out.print(i+" ");
+        
+           
+        }
+        System.out.println();
+        //for(int k=1;k>=i;--k) {
+           // System.out.print(k+" ");
+           
+        } 
+           
+        }
+        
+    }
+   
+        
+    
+    
