09cda2a889c419df9eb02dae42703eff8c9bc929
sirichowdary123 <sirichowdary8742gmail.com>
2021-12-22 17:58:42 +0530

    commied a new file of day9

---
+++ RectanglePattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.*;
+public class RectanglePattern {
+    public static void main(String args[]){
+        //Scanner sc = new Scanner(System.in);
+        //int N = sc.nextInt();
+        for(int i=1;i<4;i++){
+            for(int j=1;j<6;j++)
+                System.out.print("* ");
+                System.out.println(" ");
+            
+         
+            
+    }
+    
+
+    }
+}
